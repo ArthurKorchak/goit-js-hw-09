7dc230b078ddd9baf6e5c998f7ffe3c84024859c
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-11 12:15:22 +0300

    upd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let control = false;
+let timerId;
 
+startBtn.addEventListener('click', () => {
+    if (!control) {
+        timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        control = true;
+        }, 1000);
+    };
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    control = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};

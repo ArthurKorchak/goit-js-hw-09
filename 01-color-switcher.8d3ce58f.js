e3e1543b43e1d157f594d3b888b6cdaee613188b
ArthurKorchak <105571446+ArthurKorchak@users.noreply.github.com>
2022-08-11 20:47:33 +0000

    Deploying to gh-pages from @ ArthurKorchak/goit-js-hw-09@2ff329d9b74ae2b940563c3b7c5e0f41ed0c5079 🚀

---
+++ 01-color-switcher.8d3ce58f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]");let d;t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.querySelector("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.8d3ce58f.js.map
